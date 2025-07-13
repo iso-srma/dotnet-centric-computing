6205619bc9443ba31bc86887a9282c6b38d8df1f
iso-srma <isosrma@gmail.com>
2025-07-13 18:04:39 +0545

    psuh

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;  // Import system namespace
+
+// Namespace declaration (optional in small programs)
+namespace CSharpBasics
+{
+    // Class declaration
+    class Program
+    {
+        // Main method – entry point of every C# program
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Ishwor!");
+        }
+    }
+}
